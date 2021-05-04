07e20dfc4400bc011a49e39859775243fc425a6b
Kuro Hsu <kurotanshi@gmail.com>
2021-05-04 23:41:20 +0800

    add template & import styles

--- vite-cart/src/main.js
+++ vite-cart/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/main.scss';
+
 createApp(App).mount('#app')
